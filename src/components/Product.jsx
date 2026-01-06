f40210b7fc9fdc74b7d58d3bcaa5348a1ad214c5
Kursant Anarkulov <anarkursant08@gmail.com>
2026-01-06 20:04:48 +0600

    закончиль

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,16 @@
 import { products } from "../utils/constants/arry";
+import "./ProductCard.css"
 
 export function ProductList() {
   return (
-    <>
-    </>
+    <div className="box-card">
+      {products.map((item, index) => (
+        <div key={index} className="card">
+          <img src={item.image} alt={item.title}/>
+          <h3>{item.title}</h3>
+          <p>{item.price} сом.</p>
+        </div>
+      ))}
+    </div>
   );
 }
---
+++ src/components/constants/Arry.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    title: "Беспроводные наушники",
+    image:
+      "https://afm.kg/d/besprovodnye_naushniki_r9_bluetooth_4.png",
+    price: 5500
+  },
+  {
+    title: "Смарт-часы",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNdGs2h8LFcpusIj51HAoLJMyd_W7ICO459g&s",
+    price: 12000
+  },
+  {
+    title: "Игровая мышь",
+    image:
+      "https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?auto=format&fit=crop&w=600&q=80",
+    price: 3200
+  },
+  {
+    title: "Механическая клавиатура",
+    image:
+      "https://ir.ozone.ru/s3/multimedia-h/c1000/6231886277.jpg",
+    price: 8500
+  },
+  {
+    title: "Внешний аккумулятор",
+    image:
+      "https://storage.yandexcloud.net/10000mah.ru/uploads/common/2018/02/14/Wireless_powerbank_10000_mah_05.jpg",
+    price: 2100
+  },
+  {
+    title: "Чехол для ноутбука",
+    image:
+      "https://img5.lalafo.com/i/posters/api/5c/b8/76/esli-vy-vladelec-macbook-pro-ili-air-2022-2019-id-34438246-823880516.jpeg",
+    price: 1500
+  }
+];
